9927cfcebb41d6a84c99fa5be1e66593c8466772
kurow <edith95171@gmail.com>
2023-01-01 13:34:13 +0100

    Add classes: BillingAccount and SavingsAccount

---
+++ Bank/BillingAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class BillingAccount
+    {
+        public string AccountNumber;
+        public decimal Balance;
+        public string FirstName;
+        public string LastName;
+        public long IdNumber;
+
+    }
+}
