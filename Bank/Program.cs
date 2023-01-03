469fb99de33325d5dc2f3c7f51ab6abb65c297fd
kurow <edith95171@gmail.com>
2023-01-03 15:32:43 +0100

    Add print list of customers

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -20,16 +20,32 @@
             manager.CreateSavingsAccount("Magdalena", "Wodna", 88053124625);
             manager.CreateBillingAccount("Michał", "Kowalski", 92012014325);
 
+            manager.TakeMoney("4300000001", 50);
+            
             //manager.CreateBillingAccount(accounts[0].FirstName, accounts[0].LastName, accounts[0].IdNumber);
 
-            IList<Account> accounts = (IList<Account>)manager.GetAllAccounts();
+            IEnumerable<Account> accounts = manager.GetAllAccounts();
+
+            
+            
 
             IPrinter printer = new Printer();
             //IPrinter printer1= new SmallerPrinter();
 
-            printer.Print(accounts[0]);
-            printer.Print(accounts[1]);
-            printer.Print(accounts[2]);
+            foreach (Account account in accounts)
+            {
+                printer.Print(account);
+            }
+
+            
+            //Print List of Customers
+            IEnumerable<string> listofcustomers = manager.ListOfCustomers();
+            
+            foreach(string customer in listofcustomers)
+            {
+                Console.WriteLine(customer);
+            }
+
 
             Console.ReadKey();
         }
