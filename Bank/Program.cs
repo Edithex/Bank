73eb1a1846abc406ab7e36d27e44d0879cb2ee2e
kurow <edith95171@gmail.com>
2023-01-09 14:40:57 +0100

    new version

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace Bank
+﻿using Bank.Commands;
+
+namespace Bank
 {
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            BankManager bankManager = new BankManager();
-            bankManager.Run();
+        {
+            /* BankManager bankManager = new BankManager();
+             bankManager.Run();*/
+
+            AccountManager accountManager = new AccountManager();
+            Printer printer= new Printer(); 
+
+            Menu menu = new Menu(accountManager,printer);
+            menu.Run();
 
             Console.ReadKey();
         }
