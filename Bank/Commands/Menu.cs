bbdd66fa1175e7a3e977a4bd5e570ee05d44207d
kurow <edith95171@gmail.com>
2023-01-09 14:39:52 +0100

    Menu use ICommand

--- Bank/Commands/Menu.cs
+++ Bank/Commands/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,66 @@ namespace Bank.Commands
     {
         private IEnumerable<ICommand> Commands;
 
-        public Menu(AccountManager accountManager)
+        public Menu(AccountManager accountManager, Printer printer)
         {
-            Commands = new List<ICommand>();
-            
-
+            Commands = new ICommand[]
+            {
+                new ListOfAccountsCommand(accountManager, printer)
+            };
         }
        
+        public void Run()
+        {
+            string action;
+            Console.WriteLine("MENU");
+            Console.WriteLine();
+            do
+            {
+                foreach (ICommand command in Commands)
+                {
+                    Console.WriteLine("{0} - {1}", command.GetName(), command.GetDescription());
+                    Console.WriteLine();
+                }
+                    Console.Write("Wpisz nr akcji: ");
+                    action = Console.ReadLine();
+                    //if (string.IsNullOrEmpty(action))
+                    //{
+                        RunCommand(action);
+                    //}
+                
+            }
+            while (action != "0");
+
         }
+
+        public void RunCommand(string name) 
+        {
+            if (name == "0")
+                return;
+
+            ICommand selected = null;
+
+            foreach(ICommand command in Commands)
+            { 
+                if(command.GetName() == name)
+                {
+                    selected = command;
+                    break;
+                }
+            }
+
+            if(selected!= null) 
+            {
+                Console.Clear();
+                selected.Run();
+                Console.Clear();
+            }
+
+            else
+            {
+                Console.WriteLine("!!! Nieznana komenda");
+            }
+        }
+
     }
 }
