696d8629d306428db78574e31bf5326b6f84bdb3
kurow <edith95171@gmail.com>
2023-01-19 23:15:27 +0100

    Add new Command to Save Data to File

---
+++ Bank/Commands/SaveCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Commands
+{
+    internal class SaveCommand : ICommand
+    {
+        private AccountManager AccountManager;
+
+        public SaveCommand(AccountManager accountManager)
+        {
+            AccountManager = accountManager;
+        }
+
+        public string GetName()
+        {
+            return "8";
+        }
+
+        public string GetDescription()
+        {
+            return "Zapisz dane";
+        }
+
+
+        public void Run()
+        {
+            Console.Clear();
+            using (var sw = new StreamWriter("C:\\Users\\kurow\\source\\repos\\Bank\\Bank\\TextFile\\AccountsDataBase.txt"))
+            {
+                foreach (string customer in AccountManager.ListOfCustomers())
+                {
+                    sw.WriteLine(customer);
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Zapisano wprowadzone dane kont do pliku");
+            Console.ReadKey();
+        }
+    }
+}
