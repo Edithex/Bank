7bf5e6ea40883b09d06760349e4fd93df3da2bc6
kurow <edith95171@gmail.com>
2023-01-21 14:38:54 +0100

    Add check Clients data

--- Bank/Commands/CustomerData.cs
+++ Bank/Commands/CustomerData.cs
@@ -22,16 +22,37 @@ namespace Bank.Commands
 
         public static CustomerData ReadCustomerData()
         {
-            string firstName, lastName, idNumber;
-            Console.WriteLine("Podaj dane klienta:");
-            Console.Write("Imię: ");
-            firstName = Console.ReadLine();
-            Console.Write("Nazwisko: ");
-            lastName = Console.ReadLine();
-            Console.Write("PESEL: ");
-            idNumber = Console.ReadLine();
+
+                string firstName, lastName, idNumber;
+                Console.WriteLine("Podaj dane klienta:");
+                Console.Write("Imię: ");
+                firstName = Console.ReadLine();
+                while((string.IsNullOrEmpty(firstName) == true))
+                {
+                Console.WriteLine("Podaj prawidłowe dane!");
+                Console.Write("Imię: ");
+                firstName = Console.ReadLine();
+                };
+                Console.Write("Nazwisko: ");
+                lastName = Console.ReadLine();
+                while ((string.IsNullOrEmpty(lastName) == true))
+                {
+                    Console.WriteLine("Podaj prawidłowe dane!");
+                    Console.Write("Nazwisko: ");
+                    lastName=Console.ReadLine();
+                };
+
+                Console.Write("PESEL: ");
+                idNumber = Console.ReadLine();
+                while (idNumber.Length != 11)
+                {
+                Console.WriteLine("Podaj prawidłowe dane!");
+                Console.Write("PESEL: ");
+                idNumber = Console.ReadLine();
+                };
 
             return new CustomerData(firstName, lastName, idNumber);
+
         }
     }
 }
