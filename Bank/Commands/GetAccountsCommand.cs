fd1f44160aa0109060823a10c464c0ae49ec85d1
kurow <edith95171@gmail.com>
2023-01-30 23:51:36 +0100

    Command to dowload data from database sql

---
+++ Bank/Commands/GetAccountsCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Commands
+{
+    internal class GetAccountsCommand : ICommand
+    {
+        private AccountManager AccountManager;
+
+        public GetAccountsCommand(AccountManager accountManager)
+        {
+            AccountManager = accountManager;
+        }
+
+        public string GetName()
+        {
+            return "9";
+        }
+
+        public string GetDescription()
+        {
+            return "Pobierz dane";
+        }
+
+
+        public void Run()
+        {
+            AccountManager.GetAllAccountsFromDataBase();
+            Console.WriteLine("Konta oszczędnościowe:");
+            foreach (string customer in AccountManager.ListOfCustomersAccountSavings())
+            {
+                Console.WriteLine(customer);
+            }
+            Console.WriteLine("Konta rozliczeniowe:");
+            foreach (string customer in AccountManager.ListOfCustomersAccountBilling())
+            {
+                Console.WriteLine(customer);
+            }
+            Console.ReadKey();
+        }
+    }
+}
