5b3045a978a4f85dd9dcbd3ab96f3c9c2b37864a
kurow <edith95171@gmail.com>
2023-02-28 13:09:15 +0100

    Enum to easy numbering name command

---
+++ Bank/Commands/CommandsName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Commands
+{
+    enum CommandsName
+    {
+        ListOfAccounts = 1,
+        AddBillingAccounts = 2,
+        AddSavingsAccounts = 3,
+        AddMoney = 4,
+        TakeMoney = 5,
+        ListOfAllAccounts = 6,
+        CloseMonth = 7,
+        SavingToFile = 8
+    }
+}
